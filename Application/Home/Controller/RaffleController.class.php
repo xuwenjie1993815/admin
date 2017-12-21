817be1c2cbfd4f96411e49cd59e7fa3620f89b60
yanghui520 <34259231+yanghui520@users.noreply.github.com>
2017-12-21 23:48:45 +0800

    开奖
    开奖

--- Application/Home/Controller/RaffleController.class.php
+++ Application/Home/Controller/RaffleController.class.php
@@ -18,9 +18,26 @@ class RaffleController extends \Think\Controller
 	//商品抽奖
 	public function index()
 	{
-		$res = M('period')->alias("a")->field('period_id,product_name,product_info,period_time,create_time')->join("left join hyz_product as b on a.p_id = b.product_id")->where('target_num=now_num and status_period=1')->select();
+		$count = M('period')->where('target_num=now_num and status_period=1')->count();
+        $Page= new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数
+        $Page->setConfig('prev','');
+        $Page->setConfig('next','');
+        $show= $Page->show();// 分页显示输出
+		$res = M('period')->alias("a")->field('period_id,product_name,product_info,period_time,create_time')->join("left join hyz_product as b on a.p_id = b.product_id")->where('target_num=now_num and status_period=1')->order('create_time desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		$this->assign('list',$res);
-        //$this->assign('page',$show);
+        $this->assign('page',$show);
 		$this->display();
 	}
+	//开奖
+	public function prize()
+	{
+		$period_id = I('period_id');
+		$period_id = addslashes($period_id);
+		$info = M('period')->where(array('period_id'=>$period_id))->find();
+		$product_id = $info['p_id'];
+		$period_time = $info['period_time'];
+		$order = M('order')->where(array('period_time'=>$period_time,'order_product_id'=>$product_id))->select();
+		
+	}
+
 }
