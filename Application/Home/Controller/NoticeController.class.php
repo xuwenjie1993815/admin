a8ce27dfeffb96cde4efcdd79c0cc0ec41accd11
徐闻洁 <380935912@qq.com>
2017-12-21 11:38:13 +0800

    公告发布,编辑,查看,ueditor

--- Application/Home/Controller/NoticeController.class.php
+++ Application/Home/Controller/NoticeController.class.php
@@ -23,7 +23,7 @@ class NoticeController extends \Base\Controller\BaseController
 				case '1':
 					$notice_list[$key]['notice_status_name'] = '已发布';
 					break;
-				case '2':
+				case '0':
 					$notice_list[$key]['notice_status_name'] = '已删除';
 					break;
 			}
@@ -33,23 +33,98 @@ class NoticeController extends \Base\Controller\BaseController
 
 	//创建通知
 	public function createNotice(){
+		if (empty($_POST)) {
+    		$this->display();
+    		die;
+    	}
+    	if (!$_POST['notice_title']) {
+       	$data['msg'] = '标题必填';
+       	$this->error($data['msg']);
+    	}
+
+    	//标题长度限制
+    	if (strlen($_POST['notice_title']) > 60) {
+    		$data['msg'] = '标题最多30字';
+			$this->error($data['msg']);
+    	}
+		if (!$_POST['content']) {
+       	$data['msg'] = '内容必填';
+		$this->error($data['msg']);
+    	}
+    	//指定用户
+    	if ($_POST['user_tel']) {
+    		$user_info = M('user')->where(array('tel' => $_POST['user_tel'],'status' => '1'))->find();
+    		if (!$user_info) {
+    			$data['msg'] = '指定发送用户不存在或已注销';
+				$this->error($data['msg']);
+    		}
+			$data['user_id'] = $user_info['user_id'];
+    	}
+
 		$data['shop_id'] = $_SESSION['adminInfo']['id'];
 		//权限判断
 		$data['notice_title'] = $_POST['notice_title'];
 		$data['content'] = $_POST['content'];
 		$data['type'] = $_POST['type'];
-		$data['user_id'] = $_POST['user_id'];//通知用户 未0则是全部用户
 		$data['add_time'] = time();
-		if ($data['content']) {
-			$res = M('notice')->add($data);
-		}
+		$res = M('notice')->add($data);
 		if ($res) {
-			$this->success('新增成功', 'productList');
+			$this->success('新增成功', 'noticeList');
 		}else{
 			$this->error('新增失败');
 		}
 		
 	}
 
-	//删除通知
+	//通知详情页
+    function noticeInfo(){
+        $id = $_GET['notice_id'];
+        $notice_info = M('notice')->where("notice_id = '$id'")->find();
+        switch ($notice_info['type']) {
+        	case '1':
+        		$notice_info['type_name'] = '中奖通知';
+        		break;
+        	case '2':
+        		$notice_info['type_name'] = '公告';
+        		break;
+        }
+        switch ($notice_info['notice_status']) {
+        	case '1':
+        		$notice_info['status_name'] = '通知已送达';
+        		break;
+        	case '2':
+        		$notice_info['status_name'] = '通知已删除';
+        		break;
+        }
+        
+        $user_info = M('user')->where(array('user_id' => $notice_info['user_id']))->find();
+        $notice_info['user_name'] = $user_info['real_name']?:$user_info['user_name'];
+        // $notice_type = getNoticeType();
+        // $notice_info['type'] = $notice_type[$notice_info['type']];
+        $notice_info['admin_name'] = M('admin')->where(array('id' =>  $notice_info['shop_id']))->getField('nick_name');
+        // var_dump($notice_info);die;
+        $this->assign('notice_info',$notice_info);
+        $this->display();
+    }
+
+    //删除通知
+   	public function delNotice(){
+   		$return_data =array();
+        $return_data['status'] = 0;
+        if(isset($_POST['id'])){
+            $id = I('post.id');
+            $rst = M('notice')->where(array('notice_id'=>$id))->save(array('notice_status'=>0));
+            if($rst){
+                $return_data['status'] = 1;
+                $return_data['msg'] = "删除成功";
+                echo json_encode($return_data);die();
+            }else{
+                $return_data['msg'] = "删除失败";
+                echo json_encode($return_data);die();
+            }
+        }else{
+            $return_data['msg'] = "参数错误";
+            echo json_encode($return_data);die();
+        }
+   	}
 }
