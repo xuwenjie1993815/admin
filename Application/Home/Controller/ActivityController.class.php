8574210a5eb7266c0d5ced2c1e33f3177d0c53c5
徐闻洁 <380935912@qq.com>
2018-01-04 16:12:25 +0800

    后台活动添加 列表

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -26,12 +26,63 @@ class ActivityController extends \Base\Controller\BaseController
 
 	//添加活动
 	public function activityAdd(){
-		$activity_info = $_POST['activity_info'];
-		$this->display();
+		if (!$_POST) {
+			$this->display();die;
+		} 
+		
+		//活动图片，标题，内容。参与价格，点赞价格，参与活动奖励，点赞活动奖励
+		$_POST['stop_time'] ? $stop_time = strtotime($_POST['stop_time']) : $stop_time = NULL;
+		if ($_FILES['images']['size']) {
+            $images = D('Support')->upload();
+            $config_path = C('UPLOAD_PATH');
+            foreach ($images as $k => $v) {
+               $path_all[]='http://'.$_SERVER['HTTP_HOST'].$config_path.$v;
+            }
+            $images = implode(',', $path_all);
+        }else{
+        	$data['msg'] = '缺少参数';
+       		$this->error($data['msg']);
+        }
+        //判断数据
+        if ((!$_POST['title'] AND !$_POST['activity_name']) || !$_POST['target_num'] || !$_POST['stop_time'] || !$_POST['price'] || !$_POST['like_price'] || !$_POST['like_info']) {
+        	$data['msg'] = '缺少参数';
+       		$this->error($data['msg']);
+        }
+		$data['title'] = $_POST['title']?:$_POST['activity_name'];
+		$data['activity_name'] = $_POST['activity_name']?:$_POST['title'];
+		$data['images'] = $images;
+		$data['target_num'] = $_POST['target_num'];
+		$data['stop_time'] = $stop_time;
+		$data['price'] = $_POST['price'];
+		$data['like_price'] = $_POST['like_price'];
+		$data['like_info'] = $_POST['like_info'];
+		$data['activity_type'] = $_POST['activity_type'];
+		$data['activity_info'] = $_POST['activity_info'];
+		$data['shop_id'] = $_SESSION['adminInfo']['id'];
+		$data['ctime'] = time();
+		M('activity')->add($data);
+		$this->success('添加成功', 'activityList');
 	}
 
 	//编辑活动参数
 	public function activityInfoEdit(){
 		$activity_id = $_POST['activity_id'];
+	}
+
+	//删除活动
+	public function delActivity(){
+		$activity_id = $_POST['id'];
+		$activity_info = M('activity')->where(array('activity_id' => $activity_id))->find();
+		if ($activity_info['status'] == 0) {
+			$data['msg'] = '活动已删除';
+       		$this->error($data['msg']);
+		}
+		$ret = M('activity')->where(array('activity_id' => $activity_id))->save(array('status' => 0));
+		if ($ret) {
+			$this->success('删除成功', 'activityList');
+		}else{
+			$data['msg'] = '删除失败';
+       		$this->error($data['msg']);
+		}
 	}
 }
