6309b764267a4634f7996013aba1c76c00993d59
xuwenjie1993815 <380935912@qq.com>
2018-02-26 23:58:28 +0800

    fix

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -95,8 +95,16 @@ class ActivityController extends \Base\Controller\BaseController
 			$data['apply_real_name'] = I('name');
 			$data['sex'] = I('sex');
 			$data['age'] = I('age');
-			$data['id_card_no'] = I('card');
-			$data['tel'] = I('phone');
+			if($this->is_idcard(I('card'))){
+				$data['id_card_no'] = I('card');
+			}else{
+       		$this->error('请填写正确的身份证号码');
+			}
+			if($this->is_mobile_phone(I('tel'))){
+				$data['tel'] = I('tel');
+			}else{
+       		$this->error('请填写正确的手机号码');
+			}
 			$data['company'] = I('company');
 			$data['address'] = I('address');
 			$data['job'] = I('job');
@@ -111,4 +119,74 @@ class ActivityController extends \Base\Controller\BaseController
 			$this->display();
 		}
 	}
+
+	/********************php验证身份证号码是否正确函数*********************/
+	function is_idcard( $id ){ 
+	  $id = strtoupper($id); 
+	  $regx = "/(^\d{15}$)|(^\d{17}([0-9]|X)$)/"; 
+	  $arr_split = array(); 
+	  if(!preg_match($regx, $id)) 
+	  { 
+	    return FALSE; 
+	  } 
+	  if(15==strlen($id)) //检查15位 
+	  { 
+	    $regx = "/^(\d{6})+(\d{2})+(\d{2})+(\d{2})+(\d{3})$/"; 
+	  
+	    @preg_match($regx, $id, $arr_split); 
+	    //检查生日日期是否正确 
+	    $dtm_birth = "19".$arr_split[2] . '/' . $arr_split[3]. '/' .$arr_split[4]; 
+	    if(!strtotime($dtm_birth)) 
+	    { 
+	      return FALSE; 
+	    } else { 
+	      return TRUE; 
+	    } 
+	  } 
+	  else      //检查18位 
+	  { 
+	    $regx = "/^(\d{6})+(\d{4})+(\d{2})+(\d{2})+(\d{3})([0-9]|X)$/"; 
+	    @preg_match($regx, $id, $arr_split); 
+	    $dtm_birth = $arr_split[2] . '/' . $arr_split[3]. '/' .$arr_split[4]; 
+	    if(!strtotime($dtm_birth)) //检查生日日期是否正确 
+	    { 
+	      return FALSE; 
+	    } 
+	    else
+	    { 
+	      //检验18位身份证的校验码是否正确。 
+	      //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。 
+	      $arr_int = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); 
+	      $arr_ch = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'); 
+	      $sign = 0; 
+	      for ( $i = 0; $i < 17; $i++ ) 
+	      { 
+	        $b = (int) $id{$i}; 
+	        $w = $arr_int[$i]; 
+	        $sign += $b * $w; 
+	      } 
+	      $n = $sign % 11; 
+	      $val_num = $arr_ch[$n]; 
+	      if ($val_num != substr($id,17, 1)) 
+	      { 
+	        return FALSE; 
+	      } //phpfensi.com 
+	      else
+	      { 
+	        return TRUE; 
+	      } 
+	    } 
+	  }
+	}
+
+	//手机号验证
+	function is_mobile_phone ($mobile_phone){
+	    $chars = "/^13[0-9]{1}[0-9]{8}$|15[0-9]{1}[0-9]{8}$|18[0-9]{1}[0-9]{8}$|17[0-9]{1}[0-9]{8}$/";
+	    if(preg_match($chars, $mobile_phone)){
+	        return true;
+	    }
+	    return false;
+	}
+
+
 }
